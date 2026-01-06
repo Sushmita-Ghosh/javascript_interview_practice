306909202df982d7436cde5a9e4c23a9ea3a1ef4
Sushmita Ghosh <pujaghosh251096@gmail.com>
2026-01-05 19:37:36 -0800

    Interval Cancellation, Notes

--- 30daysOfJS/day15intervalcancellation.js
+++ 30daysOfJS/day15intervalcancellation.js
@@ -2,3 +2,56 @@
  * setInterval - to run a function at a fixed interval of time until clearInterval is called, it will keep on running unlike setTimeout, which will run only once
  * clearInterval - to stop the interval
  */
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  /** call imeediately means at 0  */
+  // setTimeout(() => {
+  //     fn(...args)
+  // }, 0)
+  fn(...args);
+
+  const interval = setInterval(() => {
+    fn(...args);
+  }, t);
+
+  const cancelFn = () => {
+    clearInterval(interval);
+  };
+
+  return cancelFn;
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)
+ */
